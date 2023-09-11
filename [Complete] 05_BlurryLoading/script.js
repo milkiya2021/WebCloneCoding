3acd3044f68a9e72b8d434f3618cb8ebca4ab70c
Hebom <aplilis@naver.com>
2023-09-12 07:35:22 +0900

    Second review

--- [Complete] 05_BlurryLoading/script.js
+++ [Complete] 05_BlurryLoading/script.js
@@ -1,22 +1,20 @@
-const progress = document.querySelector(".progress");
+const loadingText = document.querySelector(".loadingText");
 const bg = document.querySelector(".bg");
 
-let load = 0;
+let num = 0;
 
-let int = setInterval(blurring, 30);
+let haha = setInterval(blurring, 50);
 
 function blurring() {
-  load++;
-
-  if (load > 99) {
-    clearInterval(int);
+  num++;
+  if (num > 99) {
+    clearInterval(haha);
   }
-
-  progress.innerText = `${load}%`;
-  progress.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  loadingText.innerText = `${num}%`;
+  loadingText.style.opacity = scale(num, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(num, 0, 100, 30, 0)}px)`;
 }
 
-const scale = function scale(number, inMin, inMax, outMin, outMax) {
+function scale(number, inMin, inMax, outMin, outMax) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-};
+}
